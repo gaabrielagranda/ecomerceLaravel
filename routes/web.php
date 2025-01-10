7336272c731163509d3acdf435b503a01cd873c7
gaabrielagranda <gabrielagranda14@gmail.com>
2025-01-10 12:24:34 +0100

    Rutas agregadas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +21,25 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+//Routes for categories
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
+
+//Routes for products
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+//Routes for cart
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/{productId}', [CartController::class, 'add'])->name('cart.add');
+Route::patch('/cart/{itemId}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('/cart/{itemId}', [CartController::class, 'remove'])->name('cart.remove');
+
+//Routes for orders
+Route::middleware('auth')->group(function () {
+    Route::post('/checkout', [OrderController::class, 'checkout'])->name('orders.checkout');
+    Route::get('/orders/{id}', [OrderController::class, 'show'])->name('orders.show');
+});
+
 require __DIR__.'/auth.php';
+
